53c1210a431f4a4e81efdd9cff34bb2ee2b8d8b9
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-04-20 11:30:46 +0300

    fix fetchCount.

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,16 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { refs } from './get-refs';
+
 
 const URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(name) {
-
+  /*
+   * Отправляем запрос на сервер
+   * Если ответ не 200 возвращаем объект данных
+   */
   return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`).then(response => {
     if (!response.ok) {
-        Notify.failure('Oops, there is no country with that name');
+      Notify.failure('Oops, there is no country with that name');
     }
     return response.json();
   });
